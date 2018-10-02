malcolmholmes/cortex

Instrument test exporter for OpenTracing.
Also:
- Move SpanLogger out of chunk package and reuse it.
- Update the logging in the test-exporter to use gokit.

Signed-off-by: Tom Wilkie <tom.wilkie@gmail.com>

cmd/test-exporter/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"flag"
 	"math"
+	"os"
 	"time"
 
+	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
-	log "github.com/sirupsen/logrus"
-	"github.com/weaveworks/common/server"
 
+	"github.com/weaveworks/common/server"
+	"github.com/weaveworks/common/tracing"
 	"github.com/weaveworks/cortex/pkg/querier/correctness"
+	"github.com/weaveworks/cortex/pkg/util"
 )
 
 var (
@@ -25,15 +28,23 @@ func main() {
 	runnerConfig.RegisterFlags(flag.CommandLine)
 	flag.Parse()
 
+	// Setting the environment variable JAEGER_AGENT_HOST enables tracing
+	trace := tracing.NewFromEnv("test-exporter")
+	defer trace.Close()
+
+	util.InitLogger(&serverConfig)
+
 	server, err := server.New(serverConfig)
 	if err != nil {
-		log.Fatal(err)
+		level.Error(util.Logger).Log("msg", "error initializing server", "err", err)
+		os.Exit(1)
 	}
 	defer server.Shutdown()
 
 	runner, err := correctness.NewRunner(runnerConfig)
 	if err != nil {
-		log.Fatal(err)
+		level.Error(util.Logger).Log("msg", "error initializing runner", "err", err)
+		os.Exit(1)
 	}
 	defer runner.Stop()
 

pkg/chunk/chunk_store.go
@@ -17,6 +17,7 @@ import (
 	"github.com/weaveworks/cortex/pkg/chunk/cache"
 	"github.com/weaveworks/cortex/pkg/util"
 	"github.com/weaveworks/cortex/pkg/util/extract"
+	"github.com/weaveworks/cortex/pkg/util/spanlogger"
 )
 
 var (
@@ -164,7 +165,7 @@ func (c *store) calculateIndexEntries(userID string, from, through model.Time, c
 
 // Get implements Store
 func (c *store) Get(ctx context.Context, from, through model.Time, allMatchers ...*labels.Matcher) ([]Chunk, error) {
-	log, ctx := newSpanLogger(ctx, "ChunkStore.Get")
+	log, ctx := spanlogger.New(ctx, "ChunkStore.Get")
 	defer log.Span.Finish()
 	level.Debug(log).Log("from", from, "through", through, "matchers", len(allMatchers))
 
@@ -187,7 +188,7 @@ func (c *store) Get(ctx context.Context, from, through model.Time, allMatchers .
 }
 
 func (c *store) validateQuery(ctx context.Context, from model.Time, through *model.Time) (shortcut bool, err error) {
-	log, ctx := newSpanLogger(ctx, "store.validateQuery")
+	log, ctx := spanlogger.New(ctx, "store.validateQuery")
 	defer log.Span.Finish()
 
 	now := model.Now()
@@ -220,7 +221,7 @@ func (c *store) validateQuery(ctx context.Context, from model.Time, through *mod
 }
 
 func (c *store) getMetricNameChunks(ctx context.Context, from, through model.Time, allMatchers []*labels.Matcher, metricName string) ([]Chunk, error) {
-	log, ctx := newSpanLogger(ctx, "ChunkStore.getMetricNameChunks")
+	log, ctx := spanlogger.New(ctx, "ChunkStore.getMetricNameChunks")
 	defer log.Finish()
 	level.Debug(log).Log("from", from, "through", through, "metricName", metricName, "matchers", len(allMatchers))
 
@@ -253,7 +254,7 @@ func (c *store) getMetricNameChunks(ctx context.Context, from, through model.Tim
 }
 
 func (c *store) lookupChunksByMetricName(ctx context.Context, from, through model.Time, matchers []*labels.Matcher, metricName string) ([]Chunk, error) {
-	log, ctx := newSpanLogger(ctx, "ChunkStore.lookupChunksByMetricName")
+	log, ctx := spanlogger.New(ctx, "ChunkStore.lookupChunksByMetricName")
 	defer log.Finish()
 
 	userID, err := user.ExtractOrgID(ctx)

pkg/chunk/chunk_store_utils.go
@@ -4,16 +4,14 @@ import (
 	"context"
 	"sync"
 
-	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
-	ot "github.com/opentracing/opentracing-go"
-	otlog "github.com/opentracing/opentracing-go/log"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/prometheus/prometheus/promql"
 
 	"github.com/weaveworks/cortex/pkg/chunk/cache"
 	"github.com/weaveworks/cortex/pkg/util"
+	"github.com/weaveworks/cortex/pkg/util/spanlogger"
 )
 
 const chunkDecodeParallelism = 16
@@ -43,34 +41,6 @@ outer:
 		filteredChunks = append(filteredChunks, chunk)
 	}
 	return filteredChunks
-}
-
-// spanLogger unifies tracing and logging, to reduce repetition.
-type spanLogger struct {
-	log.Logger
-	ot.Span
-}
-
-func newSpanLogger(ctx context.Context, method string, kvps ...interface{}) (*spanLogger, context.Context) {
-	span, ctx := ot.StartSpanFromContext(ctx, method)
-	logger := &spanLogger{
-		Logger: log.With(util.WithContext(ctx, util.Logger), "method", method),
-		Span:   span,
-	}
-	if len(kvps) > 0 {
-		logger.Log(kvps...)
-	}
-	return logger, ctx
-}
-
-func (s *spanLogger) Log(kvps ...interface{}) error {
-	s.Logger.Log(kvps...)
-	fields, err := otlog.InterleavedKVToFields(kvps...)
-	if err != nil {
-		return err
-	}
-	s.Span.LogFields(fields...)
-	return nil
 }
 
 // Fetcher deals with fetching chunk contents from the cache/store,
@@ -139,7 +109,7 @@ func (c *Fetcher) worker() {
 
 // FetchChunks fetchers a set of chunks from cache and store.
 func (c *Fetcher) FetchChunks(ctx context.Context, chunks []Chunk, keys []string) ([]Chunk, error) {
-	log, ctx := newSpanLogger(ctx, "ChunkStore.fetchChunks")
+	log, ctx := spanlogger.New(ctx, "ChunkStore.fetchChunks")
 	defer log.Span.Finish()
 
 	// Now fetch the actual chunk data from Memcache / S3

pkg/chunk/series_store.go
@@ -15,6 +15,7 @@ import (
 	"github.com/weaveworks/cortex/pkg/chunk/cache"
 	"github.com/weaveworks/cortex/pkg/util"
 	"github.com/weaveworks/cortex/pkg/util/extract"
+	"github.com/weaveworks/cortex/pkg/util/spanlogger"
 )
 
 var (
@@ -74,7 +75,7 @@ func newSeriesStore(cfg StoreConfig, schema Schema, storage StorageClient) (Stor
 
 // Get implements Store
 func (c *seriesStore) Get(ctx context.Context, from, through model.Time, allMatchers ...*labels.Matcher) ([]Chunk, error) {
-	log, ctx := newSpanLogger(ctx, "ChunkStore.Get")
+	log, ctx := spanlogger.New(ctx, "ChunkStore.Get")
 	defer log.Span.Finish()
 	level.Debug(log).Log("from", from, "through", through, "matchers", len(allMatchers))
 
@@ -138,7 +139,7 @@ func (c *seriesStore) Get(ctx context.Context, from, through model.Time, allMatc
 }
 
 func (c *seriesStore) lookupSeriesByMetricNameMatchers(ctx context.Context, from, through model.Time, metricName string, matchers []*labels.Matcher) ([]string, error) {
-	log, ctx := newSpanLogger(ctx, "ChunkStore.lookupSeriesByMetricNameMatchers", "metricName", metricName, "matchers", len(matchers))
+	log, ctx := spanlogger.New(ctx, "ChunkStore.lookupSeriesByMetricNameMatchers", "metricName", metricName, "matchers", len(matchers))
 	defer log.Span.Finish()
 
 	// Just get series for metric if there are no matchers
@@ -202,7 +203,7 @@ func (c *seriesStore) lookupSeriesByMetricNameMatchers(ctx context.Context, from
 }
 
 func (c *seriesStore) lookupSeriesByMetricNameMatcher(ctx context.Context, from, through model.Time, metricName string, matcher *labels.Matcher) ([]string, error) {
-	log, ctx := newSpanLogger(ctx, "ChunkStore.lookupSeriesByMetricNameMatcher", "metricName", metricName, "matcher", matcher)
+	log, ctx := spanlogger.New(ctx, "ChunkStore.lookupSeriesByMetricNameMatcher", "metricName", metricName, "matcher", matcher)
 	defer log.Span.Finish()
 
 	userID, err := user.ExtractOrgID(ctx)
@@ -263,7 +264,7 @@ func (c *seriesStore) lookupSeriesByMetricNameMatcher(ctx context.Context, from,
 }
 
 func (c *seriesStore) lookupChunksBySeries(ctx context.Context, from, through model.Time, seriesIDs []string) ([]string, error) {
-	log, ctx := newSpanLogger(ctx, "ChunkStore.lookupChunksBySeries")
+	log, ctx := spanlogger.New(ctx, "ChunkStore.lookupChunksBySeries")
 	defer log.Span.Finish()
 
 	userID, err := user.ExtractOrgID(ctx)

pkg/querier/correctness/simple.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/client_golang/api/prometheus/v1"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/model"
-	log "github.com/sirupsen/logrus"
+	"github.com/weaveworks/cortex/pkg/util/spanlogger"
 )
 
 const (
@@ -41,14 +42,21 @@ func NewSimpleTestCase(name string, f func(time.Time) float64) Case {
 	}
 }
 
+func (tc *simpleTestCase) Name() string {
+	return tc.name
+}
+
 func (tc *simpleTestCase) ExpectedValueAt(t time.Time) float64 {
 	return tc.expectedValueAt(t)
 }
 
 func (tc *simpleTestCase) Query(ctx context.Context, client v1.API, selectors string, start time.Time, duration time.Duration) ([]model.SamplePair, error) {
+	log, ctx := spanlogger.New(ctx, "simpleTestCase.Query")
+	defer log.Finish()
+
 	metricName := prometheus.BuildFQName(namespace, subsystem, tc.name)
 	query := fmt.Sprintf("%s{%s}[%dm]", metricName, selectors, duration/time.Minute)
-	log.Println(query, "@", start)
+	level.Info(log).Log("query", query)
 
 	value, err := client.Query(ctx, query, start)
 	if err != nil {

pkg/util/spanlogger/spanlogger.go
@@ -1,0 +1,40 @@
+package spanlogger
+
+import (
+	"context"
+
+	"github.com/go-kit/kit/log"
+	opentracing "github.com/opentracing/opentracing-go"
+	otlog "github.com/opentracing/opentracing-go/log"
+
+	"github.com/weaveworks/cortex/pkg/util"
+)
+
+// SpanLogger unifies tracing and logging, to reduce repetition.
+type SpanLogger struct {
+	log.Logger
+	opentracing.Span
+}
+
+// New makes a new SpanLogger.
+func New(ctx context.Context, method string, kvps ...interface{}) (*SpanLogger, context.Context) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, method)
+	logger := &SpanLogger{
+		Logger: log.With(util.WithContext(ctx, util.Logger), "method", method),
+		Span:   span,
+	}
+	if len(kvps) > 0 {
+		logger.Log(kvps...)
+	}
+	return logger, ctx
+}
+
+func (s *SpanLogger) Log(kvps ...interface{}) error {
+	s.Logger.Log(kvps...)
+	fields, err := otlog.InterleavedKVToFields(kvps...)
+	if err != nil {
+		return err
+	}
+	s.Span.LogFields(fields...)
+	return nil
+}

lekmitiralf/CompiledMimir

cmd/test-exporter/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"flag"
 	"math"
+	"os"
 	"time"
 
+	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
-	log "github.com/sirupsen/logrus"
-	"github.com/weaveworks/common/server"
 
 	"github.com/cortexproject/cortex/pkg/querier/correctness"
+	"github.com/cortexproject/cortex/pkg/util"
+	"github.com/weaveworks/common/server"
+	"github.com/weaveworks/common/tracing"
 )
 
 var (
@@ -25,15 +28,23 @@ func main() {
 	runnerConfig.RegisterFlags(flag.CommandLine)
 	flag.Parse()
 
+	// Setting the environment variable JAEGER_AGENT_HOST enables tracing
+	trace := tracing.NewFromEnv("test-exporter")
+	defer trace.Close()
+
+	util.InitLogger(&serverConfig)
+
 	server, err := server.New(serverConfig)
 	if err != nil {
-		log.Fatal(err)
+		level.Error(util.Logger).Log("msg", "error initializing server", "err", err)
+		os.Exit(1)
 	}
 	defer server.Shutdown()
 
 	runner, err := correctness.NewRunner(runnerConfig)
 	if err != nil {
-		log.Fatal(err)
+		level.Error(util.Logger).Log("msg", "error initializing runner", "err", err)
+		os.Exit(1)
 	}
 	defer runner.Stop()
 

pkg/chunk/chunk_store.go
@@ -17,6 +17,7 @@ import (
 	"github.com/cortexproject/cortex/pkg/chunk/cache"
 	"github.com/cortexproject/cortex/pkg/util"
 	"github.com/cortexproject/cortex/pkg/util/extract"
+	"github.com/cortexproject/cortex/pkg/util/spanlogger"
 	"github.com/weaveworks/common/user"
 )
 
@@ -165,7 +166,7 @@ func (c *store) calculateIndexEntries(userID string, from, through model.Time, c
 
 // Get implements Store
 func (c *store) Get(ctx context.Context, from, through model.Time, allMatchers ...*labels.Matcher) ([]Chunk, error) {
-	log, ctx := newSpanLogger(ctx, "ChunkStore.Get")
+	log, ctx := spanlogger.New(ctx, "ChunkStore.Get")
 	defer log.Span.Finish()
 	level.Debug(log).Log("from", from, "through", through, "matchers", len(allMatchers))
 
@@ -188,7 +189,7 @@ func (c *store) Get(ctx context.Context, from, through model.Time, allMatchers .
 }
 
 func (c *store) validateQuery(ctx context.Context, from model.Time, through *model.Time) (shortcut bool, err error) {
-	log, ctx := newSpanLogger(ctx, "store.validateQuery")
+	log, ctx := spanlogger.New(ctx, "store.validateQuery")
 	defer log.Span.Finish()
 
 	now := model.Now()
@@ -221,7 +222,7 @@ func (c *store) validateQuery(ctx context.Context, from model.Time, through *mod
 }
 
 func (c *store) getMetricNameChunks(ctx context.Context, from, through model.Time, allMatchers []*labels.Matcher, metricName string) ([]Chunk, error) {
-	log, ctx := newSpanLogger(ctx, "ChunkStore.getMetricNameChunks")
+	log, ctx := spanlogger.New(ctx, "ChunkStore.getMetricNameChunks")
 	defer log.Finish()
 	level.Debug(log).Log("from", from, "through", through, "metricName", metricName, "matchers", len(allMatchers))
 
@@ -254,7 +255,7 @@ func (c *store) getMetricNameChunks(ctx context.Context, from, through model.Tim
 }
 
 func (c *store) lookupChunksByMetricName(ctx context.Context, from, through model.Time, matchers []*labels.Matcher, metricName string) ([]Chunk, error) {
-	log, ctx := newSpanLogger(ctx, "ChunkStore.lookupChunksByMetricName")
+	log, ctx := spanlogger.New(ctx, "ChunkStore.lookupChunksByMetricName")
 	defer log.Finish()
 
 	userID, err := user.ExtractOrgID(ctx)

pkg/chunk/chunk_store_utils.go
@@ -4,16 +4,14 @@ import (
 	"context"
 	"sync"
 
-	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
-	ot "github.com/opentracing/opentracing-go"
-	otlog "github.com/opentracing/opentracing-go/log"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/prometheus/prometheus/promql"
 
 	"github.com/cortexproject/cortex/pkg/chunk/cache"
 	"github.com/cortexproject/cortex/pkg/util"
+	"github.com/cortexproject/cortex/pkg/util/spanlogger"
 )
 
 const chunkDecodeParallelism = 16
@@ -43,34 +41,6 @@ outer:
 		filteredChunks = append(filteredChunks, chunk)
 	}
 	return filteredChunks
-}
-
-// spanLogger unifies tracing and logging, to reduce repetition.
-type spanLogger struct {
-	log.Logger
-	ot.Span
-}
-
-func newSpanLogger(ctx context.Context, method string, kvps ...interface{}) (*spanLogger, context.Context) {
-	span, ctx := ot.StartSpanFromContext(ctx, method)
-	logger := &spanLogger{
-		Logger: log.With(util.WithContext(ctx, util.Logger), "method", method),
-		Span:   span,
-	}
-	if len(kvps) > 0 {
-		logger.Log(kvps...)
-	}
-	return logger, ctx
-}
-
-func (s *spanLogger) Log(kvps ...interface{}) error {
-	s.Logger.Log(kvps...)
-	fields, err := otlog.InterleavedKVToFields(kvps...)
-	if err != nil {
-		return err
-	}
-	s.Span.LogFields(fields...)
-	return nil
 }
 
 // Fetcher deals with fetching chunk contents from the cache/store,
@@ -139,7 +109,7 @@ func (c *Fetcher) worker() {
 
 // FetchChunks fetchers a set of chunks from cache and store.
 func (c *Fetcher) FetchChunks(ctx context.Context, chunks []Chunk, keys []string) ([]Chunk, error) {
-	log, ctx := newSpanLogger(ctx, "ChunkStore.fetchChunks")
+	log, ctx := spanlogger.New(ctx, "ChunkStore.fetchChunks")
 	defer log.Span.Finish()
 
 	// Now fetch the actual chunk data from Memcache / S3

pkg/chunk/series_store.go
@@ -14,6 +14,7 @@ import (
 	"github.com/cortexproject/cortex/pkg/chunk/cache"
 	"github.com/cortexproject/cortex/pkg/util"
 	"github.com/cortexproject/cortex/pkg/util/extract"
+	"github.com/cortexproject/cortex/pkg/util/spanlogger"
 	"github.com/weaveworks/common/user"
 )
 
@@ -74,7 +75,7 @@ func newSeriesStore(cfg StoreConfig, schema Schema, storage StorageClient) (Stor
 
 // Get implements Store
 func (c *seriesStore) Get(ctx context.Context, from, through model.Time, allMatchers ...*labels.Matcher) ([]Chunk, error) {
-	log, ctx := newSpanLogger(ctx, "SeriesStore.Get")
+	log, ctx := spanlogger.New(ctx, "SeriesStore.Get")
 	defer log.Span.Finish()
 	level.Debug(log).Log("from", from, "through", through, "matchers", len(allMatchers))
 
@@ -138,7 +139,7 @@ func (c *seriesStore) Get(ctx context.Context, from, through model.Time, allMatc
 }
 
 func (c *seriesStore) lookupSeriesByMetricNameMatchers(ctx context.Context, from, through model.Time, metricName string, matchers []*labels.Matcher) ([]string, error) {
-	log, ctx := newSpanLogger(ctx, "SeriesStore.lookupSeriesByMetricNameMatchers", "metricName", metricName, "matchers", len(matchers))
+	log, ctx := spanlogger.New(ctx, "SeriesStore.lookupSeriesByMetricNameMatchers", "metricName", metricName, "matchers", len(matchers))
 	defer log.Span.Finish()
 
 	// Just get series for metric if there are no matchers
@@ -202,7 +203,7 @@ func (c *seriesStore) lookupSeriesByMetricNameMatchers(ctx context.Context, from
 }
 
 func (c *seriesStore) lookupSeriesByMetricNameMatcher(ctx context.Context, from, through model.Time, metricName string, matcher *labels.Matcher) ([]string, error) {
-	log, ctx := newSpanLogger(ctx, "SeriesStore.lookupSeriesByMetricNameMatcher", "metricName", metricName, "matcher", matcher)
+	log, ctx := spanlogger.New(ctx, "SeriesStore.lookupSeriesByMetricNameMatcher", "metricName", metricName, "matcher", matcher)
 	defer log.Span.Finish()
 
 	userID, err := user.ExtractOrgID(ctx)
@@ -263,7 +264,7 @@ func (c *seriesStore) lookupSeriesByMetricNameMatcher(ctx context.Context, from,
 }
 
 func (c *seriesStore) lookupChunksBySeries(ctx context.Context, from, through model.Time, seriesIDs []string) ([]string, error) {
-	log, ctx := newSpanLogger(ctx, "SeriesStore.lookupChunksBySeries")
+	log, ctx := spanlogger.New(ctx, "SeriesStore.lookupChunksBySeries")
 	defer log.Span.Finish()
 
 	userID, err := user.ExtractOrgID(ctx)

pkg/querier/correctness/simple.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/cortexproject/cortex/pkg/util/spanlogger"
+	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/client_golang/api/prometheus/v1"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/model"
-	log "github.com/sirupsen/logrus"
 )
 
 const (
@@ -41,14 +42,21 @@ func NewSimpleTestCase(name string, f func(time.Time) float64) Case {
 	}
 }
 
+func (tc *simpleTestCase) Name() string {
+	return tc.name
+}
+
 func (tc *simpleTestCase) ExpectedValueAt(t time.Time) float64 {
 	return tc.expectedValueAt(t)
 }
 
 func (tc *simpleTestCase) Query(ctx context.Context, client v1.API, selectors string, start time.Time, duration time.Duration) ([]model.SamplePair, error) {
+	log, ctx := spanlogger.New(ctx, "simpleTestCase.Query")
+	defer log.Finish()
+
 	metricName := prometheus.BuildFQName(namespace, subsystem, tc.name)
 	query := fmt.Sprintf("%s{%s}[%dm]", metricName, selectors, duration/time.Minute)
-	log.Println(query, "@", start)
+	level.Info(log).Log("query", query)
 
 	value, err := client.Query(ctx, query, start)
 	if err != nil {

pkg/util/spanlogger/spanlogger.go
@@ -1,0 +1,40 @@
+package spanlogger
+
+import (
+	"context"
+
+	"github.com/go-kit/kit/log"
+	opentracing "github.com/opentracing/opentracing-go"
+	otlog "github.com/opentracing/opentracing-go/log"
+
+	"github.com/cortexproject/cortex/pkg/util"
+)
+
+// SpanLogger unifies tracing and logging, to reduce repetition.
+type SpanLogger struct {
+	log.Logger
+	opentracing.Span
+}
+
+// New makes a new SpanLogger.
+func New(ctx context.Context, method string, kvps ...interface{}) (*SpanLogger, context.Context) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, method)
+	logger := &SpanLogger{
+		Logger: log.With(util.WithContext(ctx, util.Logger), "method", method),
+		Span:   span,
+	}
+	if len(kvps) > 0 {
+		logger.Log(kvps...)
+	}
+	return logger, ctx
+}
+
+func (s *SpanLogger) Log(kvps ...interface{}) error {
+	s.Logger.Log(kvps...)
+	fields, err := otlog.InterleavedKVToFields(kvps...)
+	if err != nil {
+		return err
+	}
+	s.Span.LogFields(fields...)
+	return nil
+}